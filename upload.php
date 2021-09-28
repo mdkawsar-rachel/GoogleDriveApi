4cd5c062f4853c5fb1ed371acb7de850fb38fb6b
mdkawsar-rachel <79735880+mdkawsar-rachel@users.noreply.github.com>
2021-09-28 17:22:35 +0600

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,7 +1,8 @@
 <?php
 
 //upload.php
-
+include './GoogleDriveApi.php';
+$api = new GoogleDriveApi('./credentials.json');
 if(isset($_FILES['images']))
 {
 	for($count = 0; $count < count($_FILES['images']['name']); $count++)
@@ -9,8 +10,14 @@ if(isset($_FILES['images']))
 		$extension = pathinfo($_FILES['images']['name'][$count], PATHINFO_EXTENSION);
 
 		$new_name = uniqid() . '.' . $extension;
+		
+		$fullName = $_FILES['images']['name'];
+		$mimeType = $_FILES['images']['type'];
+		$fullPath = $_FILES['images']['tmp_name'];
+		
 
-		move_uploaded_file($_FILES['images']['tmp_name'][$count], 'images/' . $new_name);
+		$api->uploadFile($fullName, $mimeType, $fullPath, $parentId = "1znA_9QuxYdVMD1FVPp-xPfirbBJRW-Dm");
+		//move_uploaded_file($_FILES['images']['tmp_name'][$count], 'images/' . $new_name);
 
 	}
 
